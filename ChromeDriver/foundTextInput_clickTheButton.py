4dd6bab71f57a71d6bd395568339cfed1e126303
GeorgeDombrovskyi <georgeDombrovskyi@gmail.com>
2023-03-23 11:27:11 +0200

    add cookies func

--- ChromeDriver/foundTextInput_clickTheButton.py
+++ ChromeDriver/foundTextInput_clickTheButton.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
-import time  # For pause
 from selenium.webdriver.common.by import By
+import time  # For pause
+from auth_data import *  # Or we can import some exactly Var
+from selenium.webdriver.common.keys import Keys  # For keyboard Buttons
 
-from auth_data import *
 
-# from auth_data import text_one
 
 
 # link
 link = "https://www.google.com/"
+
 
 # Defined our Browser
 browser = webdriver.Chrome()
@@ -16,7 +17,7 @@ browser = webdriver.Chrome()
 try:
     browser.get(link)
     # Give 2 seconds pause - for give a time to document for loading
-    time.sleep(2)
+    time.sleep(1)
 
     # defined the web element for XPATH (in our case this is INPUT)
     our_input = browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
@@ -26,6 +27,9 @@ try:
 
     # insert in Input Area our text, and some text from another document (auth_data.py)
     our_input.send_keys('first word + ', text_one)
+
+    # Push ENTER button on keyboard
+    our_input.send_keys(Keys.ENTER)
 
     # Gave 5 seconds pause - for look our result, cause in another case window will close very quickly
     time.sleep(5)
--- ChromeDriver/foundTextInput_clickTheButton2.py
+++ ChromeDriver/foundTextInput_clickTheButton2.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 import time  # For pause
 from selenium.webdriver.common.by import By
+
 from auth_data import *
 # from auth_data import text_one
 
+
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 
